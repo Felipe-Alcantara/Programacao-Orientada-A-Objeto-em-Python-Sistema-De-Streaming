d993bd8b757ce867c36f2d8bfedd3542629572bf
Felipe Alcântara <FelipeTheFeh@gmail.com>
2024-04-27 02:10:47 -0300

    Adição de Identificador Único de Cliente e Melhorias na Lógica de Salvamento
    Esta commit introduz um identificador único para cada cliente, melhorando a organização dos registros no arquivo de clientes. Além disso, otimiza a lógica de salvamento, garantindo que novos clientes sejam corretamente numerados sequencialmente. Essas mudanças visam aprimorar a manutenção e a integridade dos dados do sistema.

--- Netflix.py
+++ Netflix.py
@@ -1,6 +1,8 @@
 # Vamos criar um exemplo usando a Netflix
 
 class Cliente:
+    numero_Cliente = 0
+
     def __init__(self, Nome, Email, Plano) -> None:
         self.Nome = Nome
         self.Email = Email
@@ -11,7 +13,9 @@ class Cliente:
             self.salvar_cliente()
 
     def __str__(self) -> str:
-        return "Nome: {}\nEmail: {}\nPlano: {}".format(self.Nome, self.Email, self.Plano)
+        return f"Cliente {self.numero_Cliente}: Nome: {self.Nome}\nEmail: {self.Email}\nPlano: {self.Plano}"
+
+    
     def plano_invalido(self, Plano): #Essa função verifica se o plano indicado para o cliente é valido, caso contrário, o cliente não é criado ("Erro de plano")
         if Plano in self.Planos_disponíveis:
             self.Plano = Plano
@@ -36,13 +40,28 @@ class Cliente:
             print(f"Filme: {filme.nomeFilme} para o usuário: {self.Nome} está: indisponível")
 
     def salvar_cliente(self):
-        with open("Clientes.txt", "a", encoding='utf-8') as f:
-            f.write(f"Cliente: {self.Nome}, Email: {self.Email}, Plano: {self.Plano}\n")
+        try:
+            with open("Clientes.txt", "r", encoding="utf-8") as f:
+                lines = f.readlines()
+                if lines:
+                    last_line = lines[-1]
+                    last_Cliente_num = int(last_line.split(":") [0].split()[-1])
+                    self.numero_Cliente = last_Cliente_num + 1
+                else:
+                    self.numero_Cliente = 1
+        except FileNotFoundError:
+            self.numero_Cliente = 1
+        
+        with open("Clientes.txt", "a", encoding="utf-8") as f:
+            f.write(f"Cliente {self.numero_Cliente}: {self.Nome}, Email: {self.Email}, Plano: {self.Plano}\n")
 
     def cliente_existente(self):
         try:
             with open("Clientes.txt", "r", encoding="utf-8") as f:
-                return f"Cliente: {self.Nome}, Email: {self.Email}, Plano: {self.Plano}\n" in f.read()
+                for line in f:
+                    if self.Nome in line and self.Email in line and self.Plano in line:
+                        return True
+            return False
         except FileNotFoundError:
             return False
 
@@ -56,24 +75,29 @@ class Filme:
 try:
     PrimeiroCliente = Cliente("Felipe", "Felipe@Gmail.com", "Basic")
     print(PrimeiroCliente.__str__())
+    print()
 # Cliente.add_to_database #Para adicionar as informações ao banco de dados
 except Exception as e:
     print("Erro: ", e) 
 
 try:
     SegundoCliente = Cliente("Júlio", "Júlio@Gmail.com", "Premium")
-    print(PrimeiroCliente.__str__())
+    print(SegundoCliente.__str__())
+    print()
 except Exception as e:
     print("Erro: ", e) 
 
 Filme_Premium = Filme("Filme teste", "Livre", "Premium")
+print()
 
 # Testando acesso ao filme pelo plano
 PrimeiroCliente.ver_filme(Filme_Premium)
+print()
 
 # Botão upgrade:
 PrimeiroCliente.mudar_plano("Premium")
 print(f"Seu plano atual agora é: {PrimeiroCliente.Plano}")
+print()
 
 # Testando acesso ao filme pelo plano
 PrimeiroCliente.ver_filme(Filme_Premium)
